5a6c74d55b95a6ba4f6836a1be5f3a933977f70a
djoume salvetti <djoume.salvetti@points.com>
2013-06-06 10:55:56 -0400

    DCORE-589 Adds stub method to generate authentication headers

---
+++ pylcp/mac.py
@@ -0,0 +1,11 @@
+def generate_authorization_header_value(
+        http_method,
+        host,
+        port,
+        request_path,
+        mac_key_identifier,
+        mac_key,
+        content_type,
+        body):
+
+    return "NOT IMPLEMENTED"
