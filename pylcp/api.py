83f45a0f2db5fbed56d9d18ec3392ca2bb02f25f
Taavi Burns <taavi.burns@points.com>
2013-09-25 17:00:55 -0400

    Makes lcp.api logging more verbose and pretty (closer to the actual HTTP conversation). This will help with demos!

--- pylcp/api.py
+++ pylcp/api.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 import requests
@@ -5,7 +6,21 @@ import requests
 from lcp.mac import generate_authorization_header_value
 
 
-logger = logging.getLogger(__file__)
+logger = logging.getLogger(__name__)
+request_logger = logging.getLogger(__name__ + '.request')
+response_logger = logging.getLogger(__name__ + '.response')
+
+
+def format_headers(headers):
+    return '\n'.join(
+        "{}: {}".format(k, v) for k, v in headers.items())
+
+
+def prettify_alleged_json(text):
+    try:
+        return json.dumps(json.loads(text), sort_keys=True, indent=2)
+    except:
+        return text
 
 
 class Client(object):
@@ -25,15 +40,24 @@ class Client(object):
         if not url.startswith('http'):
             url = self.base_url + url
         if self.key_id:
-            kwargs['headers']['Authorization'] = generate_authorization_header_value(
-                method,
-                url,
-                self.key_id,
-                self.shared_secret,
-                kwargs['headers'].get('Content-Type', ''),
-                kwargs.get('data', '')
-            )
-        logger.debug('%s to %s with %s', method, url, kwargs)
+            kwargs['headers']['Authorization'] = \
+                generate_authorization_header_value(
+                    method,
+                    url,
+                    self.key_id,
+                    self.shared_secret,
+                    kwargs['headers'].get('Content-Type', ''),
+                    kwargs.get('data', '')
+                )
+        request_logger.warning(
+            '---\n%s %s HTTP/1.0\n%s\n\n%s',
+            method, url,
+            format_headers(kwargs.get('headers', {})),
+            prettify_alleged_json(kwargs.get('data', '')))
         response = requests.request(method, url, **kwargs)
-        logger.debug('Got %s status code with body: \n%s', response.status_code, response.text)
+        response_logger.warning(
+            '---\nHTTP/1.0 %d %s\n%s\n\n%s',
+            response.status_code, response.reason,
+            format_headers(response.headers),
+            prettify_alleged_json(response.text))
         return response
