f69ed77529695d0a9599d41e642db52b63745dfb
Taavi Burns <taavi.burns@points.com>
2013-09-30 17:00:40 -0400

    Makes code flake8-clean and enforces flake8 going forward.

--- tests/test_api.py
+++ tests/test_api.py
@@ -11,15 +11,13 @@ class TestApiClient(object):
     def test_request_does_not_alter_absolute_urls(self):
         for absolute_url in [
                 'http://www.points.com',
-                'https://www.points.com',
-                ]:
+                'https://www.points.com']:
             yield self._assert_calls_requests_with_url, absolute_url, absolute_url
 
     def test_request_adds_base_url_to_relative_urls(self):
         for absolute_url in [
                 'some/relative/path/',
-                '/some/absolute/path',
-                ]:
+                '/some/absolute/path']:
             yield self._assert_calls_requests_with_url, absolute_url, 'BASE_URL' + absolute_url
 
     @mock.patch('lcp.api.requests.request')
