22432da1280a753d7df14ba13e7f65cfe74996ad
gdmello <gavindmello@gmail.com>
2015-03-27 09:08:26 -0400

    Added a crud object for loyalty programs. Supports the read operation only at this time.

---
+++ tests/crud/test_loyalty_program.py
@@ -0,0 +1,25 @@
+import copy
+
+import mock
+from nose import tools
+
+from pylcp import api
+from pylcp.crud import loyalty_program
+from tests.crud import base as test_base
+
+
+class TestOrdersCRUD(object):
+    def setup(self):
+        self.mock_client = mock.create_autospec(api.Client)
+        self.lps_crud = loyalty_program.LoyaltyProgram(self.mock_client)
+
+
+    def test_get_loyalty_program(self):
+        mocked_response = test_base.mock_response(headers={}, body=test_base.SAMPLE_RESPONSE)
+        self.mock_client.get.return_value = mocked_response
+
+        response = self.lps_crud.read('lp123')
+
+        tools.assert_equal(1, self.mock_client.get.call_count)
+        self.mock_client.get.assert_called_with('/lps/lp123', data=None, params=None)
+        test_base.assert_lcp_resource(mocked_response, response)
