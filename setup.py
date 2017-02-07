c604c38794bb20f2edcaf8bb239e800fe2b042b1
requires.io <support@requires.io>
2017-02-06 19:47:13 -0500

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ REQUIREMENTS = [
     'cryptography==1.7.2',
     'idna==2.2',
     'ipaddress==1.0.18',
-    'pyasn1==0.2.1',
+    'pyasn1==0.2.2',
     'pycparser==2.17',
     'pyOpenSSL==16.2.0'
 ]
