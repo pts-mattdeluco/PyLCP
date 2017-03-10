afa723d12696c24779c2c6435779daeee5ec68f6
requires.io <support@requires.io>
2017-03-09 22:05:06 -0500

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ REQUIREMENTS = [
     'requests>=2.2.1,<3.0',
     'simplejson>=3.6.4',
     'cffi==1.9.1',
-    'cryptography==1.7.2',
+    'cryptography==1.8',
     'idna==2.5',
     'ipaddress==1.0.18',
     'pyasn1==0.2.3',
