c5ecc03c9c53afbf66a6ece2f5a7fc0834e467c9
pts-mdeluco <matt.deluco@points.com>
2017-08-04 14:33:39 -0400

    Removes CRUDError which masks the requests library HTTPError and its properties, like the request and response objects which contain information such as the status code.
    CRUDError seemed to serve no purpose other than to output an error
    message with very specific information.  However, all of that info
    (mostly contained in the request and response objects) is logged by the
    APILogger.  Given that, it makes sense to pass the requests HTTPError up
    to the client rather than breaking the exception chain with CRUDError.

--- pylcp/crud/base.py
+++ pylcp/crud/base.py
@@ -13,9 +13,6 @@ try:
 except ImportError:
     from httplib import NO_CONTENT
 
-import requests
-import simplejson as json
-
 
 class LCPResource(object):
     """Base class for loyalty domain objects for LCP services
@@ -88,55 +85,10 @@ class LCPCrud(object):
     def _resource_from_http(self, method, path, payload=None, params=None):
         response = None
 
-        try:
-            response = self._http_method(method)(path, data=payload, params=params)
-            response.raise_for_status()
-        except requests.RequestException:
-            raise CRUDError(path, method, response, **{'request_payload': payload, 'request_parameters': params})
+        response = self._http_method(method)(path, data=payload, params=params)
+        response.raise_for_status()
+
         return self.resource_class(response)
 
     def _http_method(self, method):
         return getattr(self.http_client, method.lower())
-
-
-class CRUDError(Exception):
-    def __init__(self, url, method, response, **request_kwargs):
-        formatted_request = self._format_optional_args(request_kwargs)
-
-        super(CRUDError, self).__init__(
-            "{status_code} returned.\n"
-            "Method: {method}\n"
-            "Correlation ID: {cid}\n"
-            "URL: {url}\n"
-            "{formatted_request}"
-            "Response: {response}".format(
-                url=url,
-                method=method,
-                status_code=response.status_code,
-                cid=response.headers.get('pts-lcp-cid', 'none'),
-                formatted_request=formatted_request,
-                response=response.text,
-            ))
-
-    def _format_optional_args(self, request_kwargs):
-        formatted_request = ''
-        for key in list(request_kwargs.keys()):
-            value = request_kwargs[key]
-            label = self._format_label(key)
-
-            if isinstance(value, dict):
-                formatted_value = self._format_dictionary(label, value)
-            else:
-                formatted_value = u'{}: "{}"\n'.format(label, value)
-            formatted_request += formatted_value
-
-        return formatted_request
-
-    def _format_label(self, text):
-        return text.replace('_', ' ').capitalize()
-
-    def _format_dictionary(self, label, dict_to_format):
-        formatted_dictionary = ''
-        if dict_to_format is not None:
-            formatted_dictionary = u'{}: {}\n'.format(label, json.dumps(dict_to_format, indent=2, sort_keys=True))
-        return formatted_dictionary
